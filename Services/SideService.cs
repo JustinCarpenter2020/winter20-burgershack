a2c6cce04272c27d6a57737c852dfe84417e27ac
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-18 15:22:19 -0700

    side controller

---
+++ Services/SideService.cs
@@ -0,0 +1,33 @@
+using System;
+using winter20_burgershack.Models;
+
+namespace winter20_burgershack.Services
+{
+  public class SideService
+  {
+    internal object Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object GetSideById(int sideId)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object CreateSide(Side newSide)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object EditSide(int sideId)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object DeleteSide(int sideId)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
