5f8b21192b377043f17465e948768fd716b9714a
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-18 14:19:22 -0700

    drink Controller

--- Controllers/DrinksController.cs
+++ Controllers/DrinksController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using winter20_burgershack.Models;
 using winter20_burgershack.Services;
 
 namespace winter20_burgershack.Controllers
@@ -12,5 +14,83 @@ namespace winter20_burgershack.Controllers
         {
             _ds = ds;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Drink>> Get()
+        {
+            try
+            {
+                return Ok(_ds.Get());
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpGet("{drinkId}")]
+        public ActionResult<Drink> GetDrinkById(int drinkId)
+        {
+            try
+            {
+                return Ok(_ds.GetDrinkById(drinkId));
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Drink> CreateDrink([FromBody] Drink newDrink)
+        {
+            try
+            {
+                return Ok(_ds.CreateDrink(newDrink));
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpPut("{drinkId}")]
+        public ActionResult<Drink> EditDrink([FromBody] Drink updatedDrink, int drinkId)
+        {
+            try
+            {
+                updatedDrink.Id = drinkId;
+                return Ok(_ds.EditDrink(updatedDrink));
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpDelete("{drinkId}")]
+        public ActionResult<string> DeleteDrink(int drinkId)
+        {
+            try
+            {
+                _ds.Delete(drinkId);
+                return Ok("Deleted the drink");
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+
+
+
+
+
     }
 }
--- Services/DrinkService.cs
+++ Services/DrinkService.cs
@@ -1,7 +1,36 @@
+using System;
+using System.Collections.Generic;
+using winter20_burgershack.Models;
+
 namespace winter20_burgershack.Services
 {
-    public class DrinkService
+  public class DrinkService
+  {
+    internal IEnumerable<Drink> Get()
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal Drink GetDrinkById(int drinkId)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object CreateDrink(Drink newDrink)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object EditDrink(Drink updatedDrink)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal string Delete(int drinkId)
+    {
+         throw new NotImplementedException();
+    }
+
+
+  }
 }
