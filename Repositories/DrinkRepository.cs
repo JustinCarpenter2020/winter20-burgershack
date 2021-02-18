46d2362545e262985bf8afbe8cc4e263c52a74ff
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-18 14:36:57 -0700

    service set up

--- Repositories/DrinkRepository.cs
+++ Repositories/DrinkRepository.cs
@@ -1,7 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using winter20_burgershack.Models;
+
 namespace winter20_burgershack.Repositories
 {
     public class DrinkRepository
     {
-        
+        public readonly IDbConnection _db;
+
+    public DrinkRepository(IDbConnection db)
+    {
+      _db = db;
     }
+
+    internal IEnumerable<Drink> GetAll()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Drink GetById(int drinkId)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object CreateDrink(Drink newDrink)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object EditDrink(Drink original)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void DeleteDrink(Drink drink)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
 }
